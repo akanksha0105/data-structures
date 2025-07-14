2b2f49bf1c45136199937dcf88d679ae6eaa062c
Akanksha <mittalakanksha0105@gmail.com>
2025-07-14 14:56:47 +0530

    star pattern

--- starPattern/starPattern1.js
+++ starPattern/starPattern1.js
@@ -1,3 +1,8 @@
+// ****
+// ****
+// ****
+// ****
+
 for (i = 0; i < 4; i++) {
 	let row = "";
 	for (j = 0; j < 4; j++) {
@@ -5,3 +10,63 @@ for (i = 0; i < 4; i++) {
 	}
 	console.log(row);
 }
+
+// *
+// **
+// ***
+// ****
+
+for (i = 0; i < 4; i++) {
+	let row = "";
+	for (j = 0; j <= i; j++) {
+		row = row + "*";
+	}
+	console.log(row);
+}
+
+// 1
+// 12
+// 123
+// 1234
+
+for (i = 0; i < 4; i++) {
+	let row = "";
+	for (j = 0; j <= i; j++) {
+		row = row + `${j + 1}`;
+	}
+	console.log(row);
+}
+
+// 1
+// 22
+// 333
+// 4444
+for (i = 0; i < 4; i++) {
+	let row = "";
+	for (j = 0; j <= i; j++) {
+		row = row + `${i + 1}`;
+	}
+	console.log(row);
+}
+
+
+for (i = 0; i < 4; i++) {
+	let row = "";
+	for (j = 0; j < 4-i; j++) {
+		row = row + `${j + 1}`;
+	}
+	console.log(row);
+}
+
+for (i = 0; i < 5; i++) {
+	let row = "";
+	for (j = 0; j <=i; j++) {
+		if(j%2==0)
+            row = row + "1"
+        else
+        row = row + "0"
+	}
+	console.log(row);
+}
+
+
