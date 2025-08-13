1565d15101e781cf2b29dcb106ac7b284b8d2e17
Akanksha <mittalakanksha0105@gmail.com>
2025-08-13 19:40:51 +0530

    single number

--- arrays/singleNumber.js
+++ arrays/singleNumber.js
@@ -2,7 +2,7 @@ var singleNumber = function (nums) {
 	let xor = 0;
 	for (let i = 0; i < nums.length; i++) {
 		xor = xor ^ nums[i];
-		console.log("xor : ", xor);
+		// console.log("xor : ", xor);
 	}
 	return xor;
 };
