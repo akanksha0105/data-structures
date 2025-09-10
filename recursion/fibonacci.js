ee9193d671963ef10f8788e94d00f73bf09623bf
Akanksha <mittalakanksha0105@gmail.com>
2025-09-10 13:22:27 +0530

    fibonacci

---
+++ recursion/fibonacci.js
@@ -0,0 +1,9 @@
+// find the nth number in the fibonacci sequence
+
+function fibonacci(n) {
+	if (n == 1 || n == 2) return 1;
+
+	return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(5));
